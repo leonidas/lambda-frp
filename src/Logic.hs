c24d231869845f9d49683e72410355c54706f516
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2013-03-19 17:13:28 +0200

    Gravity example

--- src/Logic.hs
+++ src/Logic.hs
@@ -1,17 +1,25 @@
 
 module Logic (logic) where
 
+import Control.Arrow
 import Control.Applicative
+
 import Control.Coroutine
+import Control.Coroutine.FRP
 
 import Lambda.OpenGL (KeyEvent(..))
 import Lambda.Vector
 import ViewModel
 
 logic :: Coroutine [KeyEvent] ViewModel
-logic = pure $ ViewModel
+logic = (ballX &&& ballY) >>^ \(x, y) -> ViewModel
     { ball = Ball
-        { ballPos    = Vec2 400 300
+        { ballPos    = Vec2 x y
         , ballRadius = 25
         }
     }
+    where
+        ballX = ballSpeed >>> integrate 100
+        ballSpeed = pure 0.3
+        ballY = gravity >>> integrate 0 >>> integrate 100
+        gravity = pure 0.01
