8db549890b0c4eadefb10647c2eeff8bd9015509
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2013-03-23 12:37:23 +0200

    The progress so far

--- src/Logic.hs
+++ src/Logic.hs
@@ -1,24 +1,53 @@
 {-# LANGUAGE Arrows #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Logic (logic) where
 
 import Prelude hiding (id, (.))
 
-import Control.Applicative (pure)
 import Control.Category
 import Control.Arrow
 
 import Control.Coroutine
 import Control.Coroutine.FRP
 
+import Graphics.UI.GLUT
+
 import Lambda.OpenGL (KeyEvent(..))
 import Lambda.Vector
 import ViewModel
 
 logic :: Coroutine [KeyEvent] ViewModel
-logic = pure $ ViewModel
+logic = proc keyEvents -> do
+
+    keyLeft  <- keyPressed (SpecialKey KeyLeft)  -< keyEvents
+    keyRight <- keyPressed (SpecialKey KeyRight) -< keyEvents
+
+    let xspeed
+            | keyLeft   = -1
+            | keyRight  =  1
+            | otherwise = 0
+
+    rec switchEvent <- watch (> 500) <<< delay 1 -< ypos
+        yspeed <- switchCurrent ySwitch 0 -< (gravity, switchEvent)
+        ypos   <- integrate 100 -< yspeed
+
+    xpos   <- integrate 100 -< xspeed
+
+    returnA -< ViewModel
         { ball = Ball
-            { ballPos    = Vec2 100 100
+            { ballPos    = Vec2 xpos ypos
             , ballRadius = 25
             }
         }
+
+    where
+        gravity = 0.01
+
+        ySwitch currentYSpeed = integrate (-0.8 * currentYSpeed)
+
+        keyPressed :: Key -> Coroutine [KeyEvent] Bool
+        keyPressed k = scanE handleKeyEvent False where
+            handleKeyEvent old (KeyEvent{..})
+                | key == k  = keyState == Down
+                | otherwise = old
