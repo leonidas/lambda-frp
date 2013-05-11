35f1ffb9364b50096c1e38785cece483fb118118
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2013-05-11 17:31:01 +0300

    Initial invaders collection

--- src/Logic.hs
+++ src/Logic.hs
@@ -11,6 +11,7 @@ import Control.Category
 
 import Control.Coroutine
 import Control.Coroutine.FRP
+import Control.Coroutine.FRP.Collections
 
 import Lambda.OpenGL (KeyEvent(..))
 import Lambda.Vector
@@ -22,15 +23,24 @@ collides (Bullet{..}) (Invader{..}) = dx < 29 && dy < 39
         dx = abs $ vx bPos - vx iPos
         dy = abs $ vy bPos - vy iPos
 
+invader :: Invader -> Coroutine () (Maybe Invader)
+invader (Invader{..}) = proc _ -> do
+    let pos   = iPos
+        frame = iFrame
+    returnA -< Just $ Invader { iPos = pos, iFrame = frame }
+
 logic :: Coroutine [KeyEvent] ViewModel
 logic = proc _ -> do
+
+    invaders <- collection initialInvaders -< ((), [])
 
     returnA -< ViewModel
         { turret   = Turret { tPos = Vec2 400 550 }
         , bullets  = Bullet { bPos = Vec2 410 370 } : []
-        , invaders = zipWith Invader invaderPos invaderFrame
+        , invaders = invaders
         }
 
     where
+        initialInvaders = map invader $ zipWith Invader invaderPos invaderFrame
         invaderPos   = Vec2 <$> [64,128..10*64] <*> [64,128..4*64]
         invaderFrame = cycle [Walk1, Walk2, Death]
